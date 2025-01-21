456492b7a956f9ba72c35b8ff3026f19200ee02c
Itai Ganot <lel@lel.bz>
2025-01-21 05:01:29 +0200

    Tests automatic update of code

--- terraform/states/environments/aws/my-aws-account/eu-north-1/dev-stockholm-1/lambda/order-verification/lambda_source_code/order_verification.py
+++ terraform/states/environments/aws/my-aws-account/eu-north-1/dev-stockholm-1/lambda/order-verification/lambda_source_code/order_verification.py
@@ -7,6 +7,7 @@ from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key
 from decimal import Decimal
 
+
 # Configure logging
 default_log_args = {
     "level": logging.DEBUG if os.environ.get("DEBUG", False) else logging.INFO,
